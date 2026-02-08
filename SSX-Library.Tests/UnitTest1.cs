75a8252ba8071a8c2a9da7c5a0cee45c69383e12
Erickson <claudespeed400@gmail.com>
2026-02-07 19:36:20 -0500

    refpack sample test

--- SSX-Library.Tests/UnitTest1.cs
+++ SSX-Library.Tests/UnitTest1.cs
@@ -1,4 +1,5 @@
-using SSX_Library.Internal.BIG;
+using System.Diagnostics;
+using SSXLibrary;
 
 namespace SSX_Library.Tests;
 
@@ -7,6 +8,8 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-        
+        var sample = File.ReadAllBytes("/home/eric/Downloads/Refpack_sample.bin");
+        var output = SSXLibrary.FileHandlers.RefpackHandler.Decompress(sample);
+        File.WriteAllBytes("/home/eric/Downloads/Decompressed_Refpack_sample.bin", output);
     }
 }
