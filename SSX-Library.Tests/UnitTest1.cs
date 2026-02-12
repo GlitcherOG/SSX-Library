c422c44ae78ec29e6cc59ed2eb0af9958c9bafe1
Erickson <claudespeed400@gmail.com>
2026-02-12 09:58:21 -0500

    testing

--- SSX-Library.Tests/UnitTest1.cs
+++ SSX-Library.Tests/UnitTest1.cs
@@ -1,8 +1,5 @@
-using System;
 using SSX_Library.Internal;
 using SSXLibrary.FileHandlers;
-using System.IO;
-using Xunit;
 
 namespace SSX_Library.Tests;
 
@@ -11,6 +8,14 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
+        string bigPath = "/home/eric/Downloads/alaska.big";
+        string oldOutputPath = "/home/eric/Downloads/alaska_old";
+        string newOutputPath = "/home/eric/Downloads/alaska_new";
+
+        string inputFolder = "/home/eric/Downloads/alaska";
+        string creationPath = "/home/eric/Downloads/alaska_new.big";
+        BIG.Create(BigType.C0FB, inputFolder, creationPath, true);
+
 
     }
 }
