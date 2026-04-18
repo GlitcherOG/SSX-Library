0b1d3d2cee48750aaade2a53b0caf924718c267f
Erickson <claudespeed400@gmail.com>
2026-04-18 17:25:35 -0400

    API Placeholders

---
+++ SSX-Library/XGT.cs
@@ -0,0 +1,23 @@
+using SSX_Library.Internal.Textures;
+using SSX_Library.Internal.Utilities;
+
+namespace SSX_Library;
+
+/// <summary>
+/// Texture files for the SSX3 Gizmondo version.
+/// </summary>
+public static class XGT
+{
+    /// <summary>
+    /// Convert an XGT texture to a PNG image.
+    /// </summary>
+    /// <param name="pixelDataPath">Path to the XGT texture who's pixel indices should be used</param>
+    /// <param name="palettePath">Path to the XGT texture who's palette should be used</param>
+    /// <param name="pngOutputPath"></param>
+    public static void ToPng(string pixelDataPath, string palettePath, string outputPngPath)
+    {
+        
+    }
+}
+
+
---
+++ SSX-Library/XTF.cs
@@ -0,0 +1,20 @@
+using SSX_Library.Internal.Textures;
+using SSX_Library.Internal.Utilities;
+
+namespace SSX_Library;
+
+/// <summary>
+/// Mipmap texture files for the SSX3 Gizmondo version.
+/// </summary>
+public static class XFT
+{
+    /// <summary>
+    /// Convert an XFT texture to a PNG image for each mipmap level.
+    /// </summary>
+    /// <param name="intputPath">Path to the XFT texture</param>
+    /// <param name="outputFolder"></param>
+    public static void ToPngs(string intputPath, string outputFolder)
+    {
+        
+    }
+}
