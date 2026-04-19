a7385b5e48b5fc20ec271ff12d562eae7fef39e6
Erickson <claudespeed400@gmail.com>
2026-04-18 20:01:28 -0400

    Clear unit test

--- SSX-Library.Tests/UnitTest1.cs
+++ SSX-Library.Tests/UnitTest1.cs
@@ -1,5 +1,5 @@
 // using SSX_Library.Internal;
-using SSX_Library;
+// using SSX_Library;
 
 namespace SSX_Library.Tests;
 
@@ -8,14 +8,6 @@ public class UnitTest1
     [Fact]
     public void Test1()
     {
-        string inputPath = "/home/eric/Projects/SSX3 Gizmondo/Extracted Assets/tracks/ABC1/abc1_terrain06.xtf";
-        string outputFolder = "/home/eric/Downloads";
-        XTF.ToPngs(inputPath, outputFolder);
-
-        // string inputPath = "/home/eric/Projects/SSX3 Gizmondo/Extracted Assets/characters/zoe_02.xgt";
-        // string inputPalettePath = "/home/eric/Projects/SSX3 Gizmondo/Extracted Assets/characters/zoe_02_low.xgt";
-        // string outputfile = "/home/eric/Downloads/zoe.png";
-        // XGT.ToPng(inputPath, inputPalettePath, outputfile);
-
+        
     }
 }
