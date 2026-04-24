7f3c9d9a5f983bc7f60fcd2636f9066fc182aabc
GlitcherOG <54088472+GlitcherOG@users.noreply.github.com>
2026-04-24 19:16:25 +1000

    XBD, XSF, Cleaning csproj

---
+++ SSX-Library/FileHandlers/LevelFiles/Tricky/Xbox/XSFHandler.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSX_Library.FileHandlers.LevelFiles.Tricky.Xbox
+{
+    public class XSFHandler
+    {
+    }
+}
